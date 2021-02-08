9f93a53c65032637d913681d4db2d7f193cfb4fa
takeshi.nakata <takeshi.nakata@mixi.co.jp>
2021-02-08 16:50:35 +0900

    update to spanner v1.13.0

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/daichirata/hammer
 go 1.13
 
 require (
-	cloud.google.com/go/spanner v1.9.0
+	cloud.google.com/go/spanner v1.13.0
 	github.com/spf13/cobra v0.0.5
-	google.golang.org/api v0.30.0
-	google.golang.org/genproto v0.0.0-20200815001618-f69a88009b70
+	google.golang.org/api v0.36.0
+	google.golang.org/genproto v0.0.0-20210113195801-ae06605f4595
 )
--- internal/hammer/diff.go
+++ internal/hammer/diff.go
@@ -27,7 +27,7 @@ func Diff(ddl1, ddl2 DDL) (DDL, error) {
 func NewDatabase(ddl DDL) (*Database, error) {
 	var tables []*Table
 
-	m := make(map[string]*Table)
+	m := make(map[spansql.ID]*Table)
 	for _, istmt := range ddl.List {
 		switch stmt := istmt.(type) {
 		case *spansql.CreateTable:
@@ -72,8 +72,8 @@ type Generator struct {
 	from *Database
 	to   *Database
 
-	dropedTable []string
-	dropedIndex []string
+	dropedTable []spansql.ID
+	dropedIndex []spansql.ID
 }
 
 func (g *Generator) GenerateDDL() DDL {
@@ -255,7 +255,7 @@ func (g *Generator) generateDDLForCreateIndex(from, to *Table) DDL {
 	return ddl
 }
 
-func (g *Generator) isDropedTable(name string) bool {
+func (g *Generator) isDropedTable(name spansql.ID) bool {
 	for _, t := range g.dropedTable {
 		if t == name {
 			return true
@@ -264,7 +264,7 @@ func (g *Generator) isDropedTable(name string) bool {
 	return false
 }
 
-func (g *Generator) isDropedIndex(name string) bool {
+func (g *Generator) isDropedIndex(name spansql.ID) bool {
 	for _, t := range g.dropedIndex {
 		if t == name {
 			return true
@@ -320,7 +320,7 @@ func (g *Generator) allowNull(col spansql.ColumnDef) spansql.ColumnDef {
 	return col
 }
 
-func (g *Generator) findTableByName(tables []*Table, name string) (table *Table, exists bool) {
+func (g *Generator) findTableByName(tables []*Table, name spansql.ID) (table *Table, exists bool) {
 	for _, t := range tables {
 		if t.Name == name {
 			table = t
@@ -331,7 +331,7 @@ func (g *Generator) findTableByName(tables []*Table, name string) (table *Table,
 	return
 }
 
-func (g *Generator) findColumnByName(cols []spansql.ColumnDef, name string) (col spansql.ColumnDef, exists bool) {
+func (g *Generator) findColumnByName(cols []spansql.ColumnDef, name spansql.ID) (col spansql.ColumnDef, exists bool) {
 	for _, c := range cols {
 		if c.Name == name {
 			col = c
@@ -342,7 +342,7 @@ func (g *Generator) findColumnByName(cols []spansql.ColumnDef, name string) (col
 	return
 }
 
-func (g *Generator) findIndexByName(indexes []*spansql.CreateIndex, name string) (index *spansql.CreateIndex, exists bool) {
+func (g *Generator) findIndexByName(indexes []*spansql.CreateIndex, name spansql.ID) (index *spansql.CreateIndex, exists bool) {
 	for _, i := range indexes {
 		if i.Name == name {
 			index = i
@@ -353,7 +353,7 @@ func (g *Generator) findIndexByName(indexes []*spansql.CreateIndex, name string)
 	return
 }
 
-func (g *Generator) findIndexByColumn(indexes []*spansql.CreateIndex, column string) []*spansql.CreateIndex {
+func (g *Generator) findIndexByColumn(indexes []*spansql.CreateIndex, column spansql.ID) []*spansql.CreateIndex {
 	result := []*spansql.CreateIndex{}
 	for _, i := range indexes {
 		for _, c := range i.Columns {
