2d994a1d04b8a07c200a9865f2ec9bd4b51c0f4c
Peter Lithammer <peter.lithammer@embark-studios.com>
2023-05-16 09:36:06 +0200

    Migrate to new protobuf message types
    https://github.com/googleapis/google-cloud-go/blob/dc3e0011784d9f55d791d70146539ac84c6bd1df/migration.md

--- internal/hammer/client.go
+++ internal/hammer/client.go
@@ -8,8 +8,8 @@ import (
 
 	"cloud.google.com/go/spanner"
 	database "cloud.google.com/go/spanner/admin/database/apiv1"
+	"cloud.google.com/go/spanner/admin/database/apiv1/databasepb"
 	"google.golang.org/api/option"
-	databasepb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
 )
 
 type Client struct {
